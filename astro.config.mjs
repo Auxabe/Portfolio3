86e74fc614482d8b52e6bb503ea0554799e6905c
auxy bikouta <bikouta.e2200816@etud.univ-ubs.fr>
2025-05-19 22:33:46 +0200

    Projet Astro propre vers Portfolio3

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+import { defineConfig } from "astro/config";
+=======
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+>>>>>>> 0c0c039911361f1c34b58350abb6fb98d0b495b3
+export default defineConfig({});
